41e8b6a975a43c88a22c012d49418295cba7b44c
Gurumoorthi R <gurumoorthirajakumar11@gmail.com>
2025-01-01 01:27:17 +0530

    update topic route changed

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,12 @@
 import TopicsList from "@/components/TopicsList"
+import Link from "next/link"
 
 export default function Home() {
   return (
    <>
     <TopicsList/>
+    <Link href={"/updateTopic"}>Click
+    </Link>
    </>
   )
 }
